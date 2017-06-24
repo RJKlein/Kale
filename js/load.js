9575b72a3b8c04aac528c80ee470397f3b6a373d
RJKlein <rj.klein@comcast.net>
2017-06-24 10:39:49 -0500

    Added ground, gravity and jumping

--- js/load.js
+++ js/load.js
@@ -4,10 +4,12 @@ var loadState= {
         
         // add a loading label on screen
         var loadLabel = game.add.text(80, 150, 'loading...',{font: '30px Courier', fill: '#ffffff'});
-        
+
+
         // load assets
         game.load.image('goal', 'assets/goal.png');
         game.load.image('sky', 'assets/IMG_0027.jpg');
+       	game.load.image('ground2', 'assets/mars.png');
         game.load.image('gameover', 'assets/gameover.jpg');
         game.load.atlas('player','assets/spritesheet.png', 'assets/sprites.json');
         
